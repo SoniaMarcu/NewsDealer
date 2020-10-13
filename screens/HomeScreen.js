9fa0c427e84e9646915f3c4ce66953af370ba35a
SoniaMarcu <marcusonia@gmail.com>
2020-10-13 17:35:09 +0200

    react native elements do not install properly on my laptop, flm,  so i commented out that piece of code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Icon } from 'react-native-elements';
+//import { Icon } from 'react-native-elements';
 class HomeScreen extends Component{
     render(){
         return (
             <View style={styles.container}>
             <Header>
-                <Icon
-                            name='three-bars'
-                            size={30}
-                            color='black'
-                            onPress={() => this.props.navigation.navigate('DrawerOpen')} />
+//                <Icon
+//                            name='three-bars'
+//                            size={30}
+//                            color='black'
+//                            onPress={() => this.props.navigation.navigate('DrawerOpen')} />
                     </Header>
 
               <Text>HomeScreen</Text>
